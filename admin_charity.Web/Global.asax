1f5f8d84c4d5695e69afa5142195bc0a0ba5211f
AceBlazer <diesel_1994@live.fr>
2018-11-04 00:37:41 +0100

    1st

---
+++ admin_charity.Web/Global.asax
@@ -0,0 +1,1 @@
+<%@ Application Language="C#" CodeBehind="Global.asax.cs" Inherits="admin_charity.Web.Global"%>
---
+++ admin_charity.Web/WebApplication.cs
@@ -0,0 +1,158 @@
+using System;
+using DevExpress.ExpressApp;
+using System.ComponentModel;
+using DevExpress.ExpressApp.Web;
+using System.Collections.Generic;
+using DevExpress.ExpressApp.Xpo;
+
+namespace admin_charity.Web {
+    // For more typical usage scenarios, be sure to check out http://documentation.devexpress.com/#Xaf/DevExpressExpressAppWebWebApplicationMembersTopicAll
+    public partial class admin_charityAspNetApplication : WebApplication {
+        private DevExpress.ExpressApp.SystemModule.SystemModule module1;
+        private DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule module2;
+        private admin_charity.Module.admin_charityModule module3;
+        private admin_charity.Module.Web.admin_charityAspNetModule module4;
+        private DevExpress.ExpressApp.Security.SecurityModule securityModule1;
+        private DevExpress.ExpressApp.Security.SecurityStrategyComplex securityStrategyComplex1;
+        private DevExpress.ExpressApp.Security.AuthenticationStandard authenticationStandard1;
+        private DevExpress.ExpressApp.AuditTrail.AuditTrailModule auditTrailModule;
+        private DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule objectsModule;
+        private DevExpress.ExpressApp.Chart.ChartModule chartModule;
+        private DevExpress.ExpressApp.Chart.Web.ChartAspNetModule chartAspNetModule;
+        private DevExpress.ExpressApp.CloneObject.CloneObjectModule cloneObjectModule;
+        private DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule conditionalAppearanceModule;
+        private DevExpress.ExpressApp.FileAttachments.Web.FileAttachmentsAspNetModule fileAttachmentsAspNetModule;
+        private DevExpress.ExpressApp.HtmlPropertyEditor.Web.HtmlPropertyEditorAspNetModule htmlPropertyEditorAspNetModule;
+        private DevExpress.ExpressApp.Kpi.KpiModule kpiModule;
+        private DevExpress.ExpressApp.Notifications.NotificationsModule notificationsModule;
+        private DevExpress.ExpressApp.Notifications.Web.NotificationsAspNetModule notificationsAspNetModule;
+        private DevExpress.ExpressApp.ReportsV2.ReportsModuleV2 reportsModuleV2;
+        private DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2 reportsAspNetModuleV2;
+        private DevExpress.ExpressApp.Scheduler.SchedulerModuleBase schedulerModuleBase;
+        private DevExpress.ExpressApp.Scheduler.Web.SchedulerAspNetModule schedulerAspNetModule;
+        private DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase scriptRecorderModuleBase;
+        private DevExpress.ExpressApp.ScriptRecorder.Web.ScriptRecorderAspNetModule scriptRecorderAspNetModule;
+        private DevExpress.ExpressApp.Validation.ValidationModule validationModule;
+        private DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule viewVariantsModule;
+
+        public admin_charityAspNetApplication() {
+            InitializeComponent();
+        }
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+            args.ObjectSpaceProvider = new XPObjectSpaceProvider(args.ConnectionString, args.Connection);
+        }
+        private void admin_charityAspNetApplication_DatabaseVersionMismatch(object sender, DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs e) {
+#if EASYTEST
+            e.Updater.Update();
+            e.Handled = true;
+#else
+            if(System.Diagnostics.Debugger.IsAttached) {
+                e.Updater.Update();
+                e.Handled = true;
+            }
+            else {
+                string message = "The application cannot connect to the specified database, because the latter doesn't exist or its version is older than that of the application.\r\n" +
+                    "This error occurred  because the automatic database update was disabled when the application was started without debugging.\r\n" +
+                    "To avoid this error, you should either start the application under Visual Studio in debug mode, or modify the " +
+                    "source code of the 'DatabaseVersionMismatch' event handler to enable automatic database update, " +
+                    "or manually create a database using the 'DBUpdater' tool.\r\n" +
+                    "Anyway, refer to the following help topics for more detailed information:\r\n" +
+                    "'Update Application and Database Versions' at http://help.devexpress.com/#Xaf/CustomDocument2795\r\n" +
+                    "'Database Security References' at http://help.devexpress.com/#Xaf/CustomDocument3237\r\n" +
+                    "If this doesn't help, please contact our Support Team at http://www.devexpress.com/Support/Center/";
+
+                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
+                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
+                }
+                throw new InvalidOperationException(message);
+            }
+#endif
+        }
+        private void InitializeComponent() {
+            this.module1 = new DevExpress.ExpressApp.SystemModule.SystemModule();
+            this.module2 = new DevExpress.ExpressApp.Web.SystemModule.SystemAspNetModule();
+            this.module3 = new admin_charity.Module.admin_charityModule();
+            this.module4 = new admin_charity.Module.Web.admin_charityAspNetModule();
+            this.securityModule1 = new DevExpress.ExpressApp.Security.SecurityModule();
+            this.securityStrategyComplex1 = new DevExpress.ExpressApp.Security.SecurityStrategyComplex();
+            this.authenticationStandard1 = new DevExpress.ExpressApp.Security.AuthenticationStandard();
+            this.auditTrailModule = new DevExpress.ExpressApp.AuditTrail.AuditTrailModule();
+            this.objectsModule = new DevExpress.ExpressApp.Objects.BusinessClassLibraryCustomizationModule();
+            this.chartModule = new DevExpress.ExpressApp.Chart.ChartModule();
+            this.chartAspNetModule = new DevExpress.ExpressApp.Chart.Web.ChartAspNetModule();
+            this.cloneObjectModule = new DevExpress.ExpressApp.CloneObject.CloneObjectModule();
+            this.conditionalAppearanceModule = new DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule();
+            this.fileAttachmentsAspNetModule = new DevExpress.ExpressApp.FileAttachments.Web.FileAttachmentsAspNetModule();
+            this.htmlPropertyEditorAspNetModule = new DevExpress.ExpressApp.HtmlPropertyEditor.Web.HtmlPropertyEditorAspNetModule();
+            this.kpiModule = new DevExpress.ExpressApp.Kpi.KpiModule();
+            this.notificationsModule = new DevExpress.ExpressApp.Notifications.NotificationsModule();
+            this.notificationsAspNetModule = new DevExpress.ExpressApp.Notifications.Web.NotificationsAspNetModule();
+            this.reportsModuleV2 = new DevExpress.ExpressApp.ReportsV2.ReportsModuleV2();
+            this.reportsAspNetModuleV2 = new DevExpress.ExpressApp.ReportsV2.Web.ReportsAspNetModuleV2();
+            this.schedulerModuleBase = new DevExpress.ExpressApp.Scheduler.SchedulerModuleBase();
+            this.schedulerAspNetModule = new DevExpress.ExpressApp.Scheduler.Web.SchedulerAspNetModule();
+            this.scriptRecorderModuleBase = new DevExpress.ExpressApp.ScriptRecorder.ScriptRecorderModuleBase();
+            this.scriptRecorderAspNetModule = new DevExpress.ExpressApp.ScriptRecorder.Web.ScriptRecorderAspNetModule();
+            this.validationModule = new DevExpress.ExpressApp.Validation.ValidationModule();
+            this.viewVariantsModule = new DevExpress.ExpressApp.ViewVariantsModule.ViewVariantsModule();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // securityStrategyComplex1
+            // 
+            this.securityStrategyComplex1.Authentication = this.authenticationStandard1;
+            this.securityStrategyComplex1.RoleType = typeof(DevExpress.ExpressApp.Security.Strategy.SecuritySystemRole);
+            this.securityStrategyComplex1.UserType = typeof(DevExpress.ExpressApp.Security.Strategy.SecuritySystemUser);
+            // 
+            // securityModule1
+            // 
+            this.securityModule1.UserType = typeof(DevExpress.ExpressApp.Security.Strategy.SecuritySystemUser);
+            // 
+            // authenticationStandard1
+            // 
+            this.authenticationStandard1.LogonParametersType = typeof(DevExpress.ExpressApp.Security.AuthenticationStandardLogonParameters);
+            //
+            // auditTrailModule
+            //
+            this.auditTrailModule.AuditDataItemPersistentType = typeof(DevExpress.Persistent.BaseImpl.AuditDataItemPersistent);
+            //
+            // reportsModuleV2
+            //
+            this.reportsModuleV2.EnableInplaceReports = true;
+            this.reportsModuleV2.ReportDataType = typeof(DevExpress.Persistent.BaseImpl.ReportDataV2);
+            this.reportsModuleV2.ShowAdditionalNavigation = false;
+            // 
+            // admin_charityAspNetApplication
+            // 
+            this.ApplicationName = "admin_charity";
+            this.CollectionsEditMode = DevExpress.ExpressApp.Editors.ViewEditMode.Edit;
+            this.Modules.Add(this.module1);
+            this.Modules.Add(this.module2);
+            this.Modules.Add(this.module3);
+            this.Modules.Add(this.module4);
+            this.Modules.Add(this.securityModule1);
+            this.Security = this.securityStrategyComplex1;
+            this.Modules.Add(this.auditTrailModule);
+            this.Modules.Add(this.objectsModule);
+            this.Modules.Add(this.chartModule);
+            this.Modules.Add(this.chartAspNetModule);
+            this.Modules.Add(this.cloneObjectModule);
+            this.Modules.Add(this.conditionalAppearanceModule);
+            this.Modules.Add(this.fileAttachmentsAspNetModule);
+            this.Modules.Add(this.htmlPropertyEditorAspNetModule);
+            this.Modules.Add(this.kpiModule);
+            this.Modules.Add(this.notificationsModule);
+            this.Modules.Add(this.notificationsAspNetModule);
+            this.Modules.Add(this.reportsModuleV2);
+            this.Modules.Add(this.reportsAspNetModuleV2);
+            this.Modules.Add(this.schedulerModuleBase);
+            this.Modules.Add(this.schedulerAspNetModule);
+            this.Modules.Add(this.scriptRecorderModuleBase);
+            this.Modules.Add(this.scriptRecorderAspNetModule);
+            this.Modules.Add(this.validationModule);
+            this.Modules.Add(this.viewVariantsModule);
+            this.DatabaseVersionMismatch += new System.EventHandler<DevExpress.ExpressApp.DatabaseVersionMismatchEventArgs>(this.admin_charityAspNetApplication_DatabaseVersionMismatch);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+
+        }
+    }
+}
